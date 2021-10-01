e1ab064685245971646beb465b0365db57cb082b
ronito19 <ronnie_1019@hotmail.com>
2021-10-01 20:20:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mensajeria/segundoActivity.java
@@ -0,0 +1,14 @@
+package com.example.mensajeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class segundoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+}
