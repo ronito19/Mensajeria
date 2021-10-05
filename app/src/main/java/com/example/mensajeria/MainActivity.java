24c46d89fa9da9ac38038cacb4e2fe12732274ef
ronito19 <ronnie_1019@hotmail.com>
2021-10-05 16:50:45 +0200

    Version1

--- app/src/main/java/com/example/mensajeria/MainActivity.java
+++ app/src/main/java/com/example/mensajeria/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mensajeria;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TEXTO1 = "ronito";
+    private static final int PETICION1 = 1;
+
+    private EditText edt_enviado1 = null;
+    private TextView txt_recibido1 = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edt_enviado1 = (EditText) findViewById(R.id.edt_enviado1);
+        txt_recibido1 = (TextView) findViewById(R.id.txt_recibido1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PETICION1)
+        {
+            if(requestCode == RESULT_OK)
+            {
+                String texto = data.getStringExtra(segundoActivity.EXTRA_RESPUESTA2);
+                txt_recibido1.setText(texto);
+                edt_enviado1.setText("");
+            }
+        }
+    }
+
+    public void enviar(View view) {
+        String texto = String.valueOf(edt_enviado1.getText());
+        Intent intent = new Intent(this, segundoActivity.class);
+        intent.putExtra(EXTRA_TEXTO1, texto);
+        // startActivity(intent);  //ESTA ES LA FORMA DE ENVIAR COSAS SIN ESPERAR RESPUESTA
+        // QUIERO AHORA INICIAR UN ACTIVITY Y ESPERAR UNA RESPUESTA
+        startActivityForResult(intent, PETICION1);
+
     }
 }
--- app/src/main/java/com/example/mensajeria/segundoActivity.java
+++ app/src/main/java/com/example/mensajeria/segundoActivity.java
@@ -2,13 +2,41 @@ package com.example.mensajeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class segundoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_RESPUESTA2 = "ronito2";
+    private TextView txt_recibido2 = null;
+    private EditText edt_enviado2 = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+        txt_recibido2 = (TextView) findViewById(R.id.txt_recibido2);
+        edt_enviado2 = (EditText) findViewById(R.id.edt_enviado2) ;
+        txt_recibido2.setVisibility(View.VISIBLE);
+        //-------------------------------------------------------------
+
+        Intent intent = getIntent();
+        if(intent != null)
+        {
+            String texto = intent.getStringExtra(MainActivity.EXTRA_TEXTO1);
+            txt_recibido2.setText(texto);
+        }
+    }
+
+    public void responder(View view) {
+        String texto = String.valueOf(edt_enviado2.getText());
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_RESPUESTA2, texto);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
